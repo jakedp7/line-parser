9eac67e535c8237d8d7803e9be1eaa5849ca3663
Jake Powell <jakedp7@gmail.com>
2015-08-31 10:30:12 -0400

    Fixed LineFormatTest to comply with new FieldFormat objects

--- src/test/java/com/powell/LineParserTests/LineFormatTest.java
+++ src/test/java/com/powell/LineParserTests/LineFormatTest.java
@@ -1,5 +1,6 @@
 package com.powell.LineParserTests;
 
+import com.powell.FixedWidthLineParser.utility.FieldFormat;
 import junit.framework.TestCase;
 import org.junit.Test;
 import com.powell.FixedWidthLineParser.utility.LineFormat;
@@ -9,28 +10,27 @@ import com.powell.FixedWidthLineParser.utility.LineFormat;
  */
 public class LineFormatTest extends TestCase {
 
-    //TODO: Update test to comply with FieldFormat class which superseded FieldBounds
     @Test
     public void testEntryMethods() throws Exception {
 
         //Create and populate an object object
         LineFormat lineFormat = new LineFormat();
-        lineFormat.addEntry("domicile", new FieldBounds(1,5));
-        lineFormat.addEntry("tripNumber", new FieldBounds(6, 10));
-        lineFormat.addEntry("startDate", new FieldBounds(11, 19));
-        lineFormat.addEntry("associatedLine", new FieldBounds(20, 23));
-        lineFormat.addEntry("tripCountInLine", new FieldBounds(24, 26));
+        lineFormat.addEntry(new FieldFormat("domicile", 1,5));
+        lineFormat.addEntry(new FieldFormat("tripNumber", 6, 10));
+        lineFormat.addEntry(new FieldFormat("startDate", 11, 19));
+        lineFormat.addEntry(new FieldFormat("associatedLine", 20, 23));
+        lineFormat.addEntry(new FieldFormat("tripCountInLine", 24, 26));
 
         //Test for correct bounds in entries
         assertEquals("addEntry/getEntryIndices failed - start index",
-                lineFormat.getEntryIndices("domicile").getStartPos(), 1);
+                lineFormat.getEntry("domicile").getStartPos(), 1);
         assertEquals("addEntry/getEntryIndices failed - end index",
-                lineFormat.getEntryIndices("tripCountInLine").getEndPos(), 26);
+                lineFormat.getEntry("tripCountInLine").getEndPos(), 26);
 
         //Test entry removal
         lineFormat.removeEntry("tripCountInLine");
         assertNull("removeEntry failed",
-                lineFormat.getEntryIndices("tripCountInLine"));
+                lineFormat.getEntry("tripCountInLine"));
 
     }
 }
